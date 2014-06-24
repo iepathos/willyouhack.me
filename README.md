2d75f4c5e4e973003ebbe8bf377cb76e38230f84
Glen Baker <iepathos@gmail.com>
2014-06-23 21:45:07 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Solutions to willyouhack.me challenges.
+
+This is the repo where I store my solutions to willyouhack.me
+
+You have my permission to use my code and solutions here for educational purposes only ;)
---
+++ week1.js
@@ -0,0 +1,16 @@
+//  willyouhack.me Week 1 Solution by Glen Baker - iepathos@gmail.com
+
+// The authentication is all in the frontend
+// in a javascript resource named week1b.js
+// The decrypt function uses simple substitution cipher.
+// User passwords are all visible in the users array.
+// The admin user is notAdmin, marked with flag 'a'
+
+// Solution is to enter into console the console a 
+// blank decrypt function like so:
+
+function decrypt(a){ return a; }
+
+// Then enter plaintext password with username into the form:
+// username: notAdmin
+// password: dllkhGlgME
